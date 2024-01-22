0b384267bb395e2a306c0119c46a3834c7b2469e
SkMag9 <79118346+SkMag9@users.noreply.github.com>
2024-01-22 01:44:17 +0100

    add zsh theme link script

--- scripts/install/zsh.install.sh
+++ scripts/install/zsh.install.sh
@@ -2,5 +2,3 @@
 
 sudo apt install zsh -y
 
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k/powerlevel10k.git ~/powerlevel10k
