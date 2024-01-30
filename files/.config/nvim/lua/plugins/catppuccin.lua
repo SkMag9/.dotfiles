9364e1c165c33b9b67e7b3b85c4896c476d69b7d
SkMag9 <79118346+SkMag9@users.noreply.github.com>
2024-01-30 19:54:12 +0100

    add neo-tree modules and config

--- files/.config/nvim/lua/plugins/catppuccin.lua
+++ files/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,7 +1,7 @@
 return {
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000, 
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
   init = function()
     require("catppuccin").setup({
       flavour = "mocha",
--- files/.config/nvim/lua/plugins/neo-tree.lua
+++ files/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,12 +6,71 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config = function () 
-    vim.keymap.set("n", "<C-n>", ":Neotree toggle filesystem reveal left<CR>", {})
+  config = function ()
+    vim.keymap.set(
+      "n",
+      "<C-n>",
+      ":Neotree toggle filesystem reveal left<CR>",
+      {}
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>e",
+      ":Neotree focus filesystem reveal left<CR>",
+      {}
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>gs",
+      ":Neotree toggle git_status reveal float<CR>",
+      {}
+    )
+
     require("neo-tree").setup({
+      close_if_last_window = false,
+      enable_git_status = true,
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      default_component_configs = {
+        modified = {
+          symbol = "[~]",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = "[A]",
+            modified = "[M]",
+            deleted = "[D]",
+            renamed = "[R]",
+            -- Status type
+            untracked = "[/]",
+            ignored = "[I]",
+            unstaged = "[U]",
+            staged = "[S]",
+            conflict = "[C]",
+          },
+        },
+      },
+      window = {
+        mappings = {
+          ["P"] = {
+            "toggle_preview", config = {
+              use_float = true,
+            }
+          },
+          ["s"] = "open_split",
+          ["v"] = "open_vsplit",
+          ["a"] = {
+            "add",
+            config = {
+              show_path = "relative"
+            }
+          },
+          
         },
       },
     })
