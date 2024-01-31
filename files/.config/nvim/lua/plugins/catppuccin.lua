c624fcf4ba4add8680485bc0b18901d9be87e36a
SkMag9 <79118346+SkMag9@users.noreply.github.com>
2024-01-31 19:03:52 +0100

    apply catppuccin theme settings

--- files/.config/nvim/lua/plugins/catppuccin.lua
+++ files/.config/nvim/lua/plugins/catppuccin.lua
@@ -2,8 +2,8 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	init = function()
-		require("catppuccin").setup({
+	config = function()
+    require("catppuccin").setup({
 			flavour = "mocha",
 			no_italic = true,
 			styles = {
@@ -13,12 +13,10 @@ return {
       integrations = {
 				cmp = true,
 				gitsigns = true,
-				nvimtree = true,
 				treesitter = true,
 			},
 		})
-	end,
-	config = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
+
+    vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
