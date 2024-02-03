ff0baa2e516ca2f1b10efd255adc225866c98c4c
SkMag9 <79118346+SkMag9@users.noreply.github.com>
2024-02-03 02:26:01 +0100

    add autopairs, add treesitter-context

---
+++ files/.config/nvim/lua/plugins/treesitter-context.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup({
+        enable = true,
+        max_lines = 5,
+      })
+    end
+  }
+}
