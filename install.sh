c6863395efd6cbaf1aad9b6c269276de29d721e3
SkMag9 <79118346+SkMag9@users.noreply.github.com>
2025-05-08 01:45:14 +0200

    fix git clone syntax

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 
 sudo dnf upgrade -y
 sudo dnf install -y ansible-core git 
-git clone https://github.com/skmag9/.dotfiles ${HOME}/.dotfiles
-cd .dotfiles 
-git checkout v2.0
-ansible-playbook --ask-become-pass main.yml
+git clone -b v2.0 https://github.com/skmag9/.dotfiles ${HOME}/.dotfiles
+cd .dotfiles && ansible-playbook --ask-become-pass main.yml
 
 
