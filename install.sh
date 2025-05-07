3f1ecf91143fd47773f6a4c0313679b4ec8ce0c0
SkMag9 <79118346+SkMag9@users.noreply.github.com>
2025-05-08 01:41:32 +0200

    fix installer for work in progress on another branch

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 sudo dnf upgrade -y
 sudo dnf install -y ansible-core git 
 git clone https://github.com/skmag9/.dotfiles ${HOME}/.dotfiles
-cd .dotfiles && ansible-playbook --ask-become-pass main.yml
+cd .dotfiles 
+git checkout v2.0
+ansible-playbook --ask-become-pass main.yml
 
 
